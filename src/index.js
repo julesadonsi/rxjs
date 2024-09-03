641237d7e12f4bd8917a4d2ec293a790734f3962
JulesAD96 <adonsijules9@gmail.com>
2024-09-04 00:32:55 +0100

    feat: Create, subscribe, execute, dispose an observable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-import { of, map } from "rxjs";
+import { Observable } from "rxjs";
 
-map((x) => x * x)(of(1, 2, 3)).subscribe((v) => console.log(`Output is: ${v}`));
+const observable = new Observable((subscriber) => {
+	subscriber.next(1);
+	subscriber.next(2);
+	subscriber.next(3);
+	setTimeout(() => {
+		subscriber.next(4);
+		subscriber.complete();
+	}, 1000);
+});
+
+console.log("just before subscribe");
+observable.subscribe({
+	next(x) {
+		console.log("got value" + x);
+	},
+	error(err) {
+		console.error("Something wrong occured: " + err);
+	},
+	complete() {
+		console.log("done");
+	},
+});
+console.log("just after subscribe");
+
+// Disposing observable
+import { from } from "rxjs";
+
+const observable$ = from([10, 20, 30]);
+const subscription = observable$.subscribe((x) => console.log(x));
+subscription.unsubscribe();
